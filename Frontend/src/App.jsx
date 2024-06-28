d741a00f2737d14028b2237e0749190955218d20
Raghul.A.R <arraghul6@gmail.com>
2024-06-28 19:41:25 +0530

    added new file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,13 +7,10 @@ import { Todos } from './components/Todos'
 
 function App() {
   const [todos,setTodos] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3000/todos")
-      .then(async function(res) {
-        const json = await res.json();
-        setTodos(json.todos)
-    })
-
+  fetch("http://localhost:3000/todos")
+    .then(async function(res) {
+      const json = await res.json();
+      setTodos(json.todos)
   })
   fetch("http://localhost:3000/todos")
     .then(async function(res) {
