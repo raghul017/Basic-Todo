65b6476bd00ac0b9c8daa62a8e09b98389a96618
Raghul.A.R <arraghul6@gmail.com>
2024-06-28 19:33:25 +0530

    added todo file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
@@ -7,6 +7,14 @@ import { Todos } from './components/Todos'
 
 function App() {
   const [todos,setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
+      .then(async function(res) {
+        const json = await res.json();
+        setTodos(json.todos)
+    })
+
+  })
   fetch("http://localhost:3000/todos")
     .then(async function(res) {
       const json = await res.json();
